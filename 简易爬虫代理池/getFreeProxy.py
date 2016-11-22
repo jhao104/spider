c0265e5100e169dd83fb93f78e926bbbf6de97c8
jhao104 <946150454@qq.com>
2016-11-22 20:41:16 +0800

    [update] fetch proxy from kuaidaili

--- 简易爬虫代理池/getFreeProxy.py
+++ 简易爬虫代理池/getFreeProxy.py
@@ -7,26 +7,32 @@ def robust(func):
     def decorate(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except:
-            print u"sorry, 抓取出错"
+        except Exception as e:
+            print u"sorry, 抓取出错。错误原因:"
+            print e
 
     return decorate
 
 
+# 快代理
+# noinspection PyPep8Naming
 @robust
-def freeProxyOne(page=5):
+def freeProxyOne(page=10):
     """
     抓取快代理IP http://www.kuaidaili.com/
     :param page: 翻页数
     :return:
     """
-    url_list = ('http://www.kuaidaili.com/proxylist/{page}/'.format(page) for page in range(1, page + 1))
+    url_list = ('http://www.kuaidaili.com/proxylist/{page}/'.format(page=page) for page in range(1, page + 1))
     # 页数不用太多， 后面的全是历史IP， 可用性不高
     for url in url_list:
         html = requests.get(url).content
-        pass
-
+        tree = etree.HTML(html)
+        proxy_list = tree.xpath('.//div[@id="index_free_list"]//tbody/tr')
+        for proxy in proxy_list:
+            yield ':'.join(proxy.xpath('./td/text()')[0:2])
 
 if __name__ == '__main__':
-    f = freeProxyOne()
+    for e in freeProxyOne():
+        print e
 

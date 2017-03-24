7d9cb29c903f2c7c0edf31bb358d076de1e2f0f1
jhao104 <946150454@qq.com>
2017-03-24 09:27:56 +0800

    [add] PyV8破解JS加密的Cookie

---
+++ PyV8破解JS加密Cookie/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     __init__.py.py  
+   Description :  
+   Author :       JHao
+   date：          2017/3/24
+-------------------------------------------------
+   Change Activity:
+                   2017/3/24: 
+-------------------------------------------------
+"""
+__author__ = 'JHao'
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == '__main__':
+    pass
